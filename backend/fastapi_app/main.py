e940e0e157c4ef9dd3636d6d3e5b9ec1a5d66980
gmlwlsdl <heejin094@naver.com>
2024-08-07 18:33:15 +0900

    brands modified, connect frontend

--- backend/fastapi_app/main.py
+++ backend/fastapi_app/main.py
@@ -1,10 +1,19 @@
 # FastAPI 앱의 진입점
 from fastapi import FastAPI
-# from routers import item_router
+from fastapi.middleware.cors import CORSMiddleware
+from routers import brands
 
 app = FastAPI()
 
-# app.include_router(item_router.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React 개발 서버 주소
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(brands.router)
 
 @app.get("/")
 async def read_root():
