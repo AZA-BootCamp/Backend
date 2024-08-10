444eb6d9498be3b846a89e25ea5168d4901bac31
gmlwlsdl <heejin094@naver.com>
2024-08-10 15:47:12 +0900

    brand feedback add, connect with frontend

--- backend/fastapi_app/main.py
+++ backend/fastapi_app/main.py
@@ -40,7 +40,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React 개발 서버 주소
+    allow_origins=["http://localhost:3000", "http://localhost:8000"],  # React 개발 서버 주소
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
